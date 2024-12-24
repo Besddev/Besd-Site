418d184c35e55a2b3af952b11e98fb514b2a526b
Besddev <besdinemail@gmail.com>
2024-12-24 19:55:55 +0200

    updated index.html and made javascript work

--- BesdSite/careers.js
+++ BesdSite/careers.js
@@ -1,25 +1,39 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 
-const careers = ['programmer', 'full-stack developer', 'music enjoyer']
+const careers = ['programmer', 'full-stack developer', 'music enjoyer'];
 
-let careerindex = 0;
-
-let characterindex = 0;
-
-updateText()
+let careerIndex = 0;
+let characterIndex = 0;
+let isDeleting = false;
 
 function updateText() {
-    characterindex++
-    //container.innerHTML = `<h1>I am a${careers[careerindex].slice(0,characterindex)}</h1>`
+    const currentCareer = careers[careerIndex];
 
-    if(characterindex === careers[careerindex].length) {
-        careerindex++
-        characterindex = 0
+    if (!isDeleting) {
+        // Typing phase Add one character
+        characterIndex++;
+    } else {
+        // Deleting phase Remove one character
+        characterIndex--;
     }
 
-    if(careerindex === careers.length) {
-        careerindex = 0;
+    // Update the text in the container
+    container.innerHTML = `<h1>I am a ${currentCareer.slice(0, characterIndex)}</h1>`;
+
+    // Switch to deleting mode when the word is fully typed
+    if (characterIndex === currentCareer.length) {
+        isDeleting = true;
     }
 
-    setTimeout(updateText, 500);
+    // Switch to typing mode for the next word when the word is fully deleted
+    if (characterIndex === 0) {
+        isDeleting = false;
+        careerIndex = (careerIndex + 1) % careers.length; // Move to the next career
+    }
+
+    // Adjust speed: Typing is faster, deleting is slower
+    const delay = isDeleting ? 100 : 200;
+    setTimeout(updateText, delay);
 }
+
+updateText();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@
 module.exports = {
     content: ["./BesdSite/*.{html,js}"],
     theme: {
+      extend: {
       colors:
       {
-        'backround': '#1D1C1D',
-        'backroundabout': '#2A292B'
+        'backround-black': '#1D1C1D',
+        'about-black': '#2A292B',
+        'about-white': '#e3d5ca',
       }
     },
+  },
     plugins: [],
   }
